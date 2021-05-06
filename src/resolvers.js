2295242e82bf3a5abb5a82487eba11a41cea6305
aiassessment <83780632+aiassessment@users.noreply.github.com>
2021-05-06 19:45:17 -0400

    Implement retrieving links by slug, as well as all links. Make CreateLink conditional on lack of matching slug.

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,15 +1,24 @@
 const resolvers = {
   Query: {
     async link(root, { slug }, { models }) {
-      return models.Link.findById(slug);
+      return models.Link.findOne({ where: { slug: slug } });
+    },
+    async allLinks(root, {}, { models }) {
+      return models.Link.findAll();
     }
   },
   Mutation: {
     async createLink(root, { url, slug }, { models }) {
-      return models.Link.create({
-        url,
-        slug
-      });
+      const matchingSlug = await models.Link.findOne({ where: { slug: slug } });
+      console.log(matchingSlug);
+      if (!matchingSlug) {
+        return models.Link.create({
+          url,
+          slug
+        });
+      } else {
+        console.log("slug taken");
+      }
     }
   }
 };
--- src/schema.js
+++ src/schema.js
@@ -8,6 +8,7 @@ const typeDefs = gql`
   }
   type Query {
     link(slug: String!): Link
+    allLinks: [Link!]!
   }
   type Mutation {
     createLink(url: String!, slug: String!): Link!
